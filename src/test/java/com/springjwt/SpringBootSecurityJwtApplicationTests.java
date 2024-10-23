a08f9c55004c2d7a533a3541eac237c52b8869dc
Annabelle <annclermont@edu.aau.at>
2024-10-23 20:03:17 +0200

    updated build ci proccess

--- src/test/java/com/springjwt/SpringBootSecurityJwtApplicationTests.java
+++ src/test/java/com/springjwt/SpringBootSecurityJwtApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
-
+    @Test
+    public void contextLoads() {
+    }
 
 }
