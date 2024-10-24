b9f6fb35b977b752c8f25215930f8cbb6b1f05c4
Annabelle <annclermont@edu.aau.at>
2024-10-24 19:51:16 +0200

    update ci

--- authentication-service/src/test/java/com/springjwt/SpringBootSecurityJwtApplicationTests.java
+++ authentication-service/src/test/java/com/springjwt/SpringBootSecurityJwtApplicationTests.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
+   
 }
